8987094c96c349ac19ee589c518a15dbaf2ec7fa
quantu-adunati <mcfaddenr.ebb@gmail.com>
2019-06-27 13:39:09 +0200

    Unit tests. Lot of TODO's

--- Facial_Recogntion/faceUnitTests.py
+++ Facial_Recogntion/faceUnitTests.py
@@ -1,2 +1,9 @@
 import unittest
-import face_rec.py
+import face_rec
+
+class TestFacialRec(unittest.TestCase):
+    #TODO Adrian need to find a way to get the video stream from here to test it.
+    print("Needs work")
+
+if __name__ == '__main__':
+    unittest.main()
