3aedfa3e4c6825b9dc923c513c3774aa7077ee86
Jarrod G <u17112631@tuks.co.za>
2019-07-08 16:59:09 +0200

    Added route version of component

---
+++ API_Adapter/mainREST.ts
@@ -0,0 +1,17 @@
+//npm install express --save
+import * as Main from "./main";
+
+var express = require("express");
+var app = express();
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
+
+app.get("/getEvents", (req, res, next) => {
+
+   Main.getEvents("primary",true,{id : true,summary:true,location:true,description:true,attendees:true},-1).then( events =>{
+    res.json(events);
+   });
+});
+  
