a649506a3ab6b94e4a86f7389bdd7c4600a4127a
Kyle <kolivier12@gmail.com>
2019-07-16 14:15:01 +0200

    Routing issues

--- Admin_Backend/BackendAPI/BackendAPI/Controllers/DefaultController.cs
+++ Admin_Backend/BackendAPI/BackendAPI/Controllers/DefaultController.cs
@@ -7,22 +7,23 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
-//using System.Web.Http.Cors;//check back
+using System.Web.Http.Cors;//check back
 using System.Data.Entity;
 using BackendAPI.Models;
 using System.Dynamic;
 
 namespace AdminAPI.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class DefaultController : ApiController
     {
-        [System.Web.Http.Route("api/Employee/searchEmployees/{id}")]
+        [System.Web.Http.Route("api/Employee/searchEmployees")]
         [System.Web.Mvc.HttpPost]
-        public List<dynamic> searchEmployees(string id)
+        public List<dynamic> searchEmployees([FromBody]string name)
         {
             ARecognitionEntities db = new ARecognitionEntities();
             db.Configuration.ProxyCreationEnabled = false;
-            return getEmployees(db.Employees.ToList(), id);
+            return getEmployees(db.Employees.ToList(), name);
         }
 
         private List<dynamic> getEmployees(List<Employee> forClient, string search)
@@ -35,14 +36,42 @@ namespace AdminAPI.Controllers
                     dynamic dynamicEmployee = new ExpandoObject();
                     dynamicEmployee.EmployeeID = emp.EmployeeID;
                     dynamicEmployee.Name = emp.Name;
-                    dynamicEmployee.IDNumber = emp.IDNumber;
+                    dynamicEmployee.Surname = emp.Surname;
                     dynamicEmployee.Email = emp.Email;
-                    dynamicEmployee.Position = emp.Position;
+                    dynamicEmployee.Password = emp.Pass;
+                    dynamicEmployee.EmpPosition = emp.EmpPosition;
                     dynamicEmployees.Add(dynamicEmployee);
                     return dynamicEmployees;
                 }
             }
             return null;
+        }
+
+        [System.Web.Http.Route("api/Employee/getAllEmployees")]
+        [System.Web.Mvc.HttpGet]
+        public List<dynamic> getAllCauses()
+        {
+            ARecognitionEntities db = new ARecognitionEntities();
+            db.Configuration.ProxyCreationEnabled = false;
+            return getEmployeesReturnList(db.Employees.ToList());
+        }
+
+        private List<dynamic> getEmployeesReturnList(List<Employee> forClient)
+        {
+            List<dynamic> dynamicEmployees = new List<dynamic>();
+            foreach (Employee emp in forClient)
+            {
+                dynamic dynamicEmployee = new ExpandoObject();
+                dynamicEmployee.EmployeeID = emp.EmployeeID;
+                dynamicEmployee.Name = emp.Name;
+                dynamicEmployee.Surname = emp.Surname;
+                dynamicEmployee.Email = emp.Email;
+                dynamicEmployee.Password = emp.Pass;
+                dynamicEmployee.EmpPosition = emp.EmpPosition;
+                dynamicEmployees.Add(dynamicEmployee);
+            }
+
+            return dynamicEmployees;
         }
     }
 }
