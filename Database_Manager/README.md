596fd5013b79a5cb3ec17c307989722443d1c5c7
Adrian-le-Grange <u17056782@tuks.co.za>
2019-07-30 17:03:52 +0200

    Implemented the database manager. Refer to readme for endpoints.

--- Database_Manager/README.md
+++ Database_Manager/README.md
@@ -1,52 +1,124 @@
-# Singularity 
+# Database Management API
 ## A-Recognition Access Control
-### Links 
-[Project Repository](https://github.com/cos301-2019-se/A-Recognition)
+### Endpoints
 
-[Project Management](https://app.zenhub.com/workspaces/a-recognition-5cc3f20307a4ab52d27abc9b/board?repos=182155877)
+#### /retrieveEncodings
+This endpoint returns all active registered users as a JSON object containing email-facialData pairs
+in the form of two paralel arrays.
+i.e index 0 of the "emails" array corresponds to index 0 of the "fd" (Facial data) array.
+
+Example:
+{
+    "emails": [
+        "email1@some.domain",
+        "email2@some.domain",
+        "email3@some.domain"
+    ],
+    "fd": [
+        [
+            -0.0893356129527092,
+            0.029628077521920204,
+            0.10922149568796158,
+            ...
+            0.03481590002775192,
+            0.03250574320554733
+        ],
+        [
+            -0.11047995835542679,
+            0.1310310810804367,
+            -0.04786147549748421,
+            ...
+            0.06657759845256805,
+            0.0011006128042936325
+        ],
+        [
+            -0.11196105182170868,
+            0.028782090172171593,
+            0.030501335859298706,
+            ...
+            0.11167395859956741,
+            0.010125321336090565
+        ]
+    ]
+}
 
 ---
 
-### Description 
-Create a system which uses facial recognition to allow users access to areas that are prohibited. The system should come with an interface which can be easily used to add new faces and their respective access rights.
+#### /register
+Endpoint used to add users to the system. The request containing the details 
+should be a JSON object as follows:
+
+Example:
+{ 
+    "email" : "email@some.domain",
+    "name" : "John",
+    "surname": "Doe",
+    "title": "Mr",
+    "fd": [123,...,456],
+    "active" : false
+}
+
+Success response:
+{
+    "status" : "Success"
+}
+
+Failure response:
+{
+    "status" : "Failure",
+    "message" : "some description of the error"
+}
 
 ---
 
-### The Team
-<img src="https://i.imgur.com/y2iKVw1m.jpg" title="source: imgur.com" />
+#### /update
+Endpoint for updating a user's details. The request should contain all user details.
 
-Alessio - Kyle - Richard - Adrian - Jarrod
+Example:
+{ 
+    "email" : "email@some.domain",
+    "name" : "John",
+    "surname": "Doe",
+    "title": "Mr",
+    "fd": [123,...,456],
+    "active" : true
+}
 
-##### Jarrod Goschen
-- Software development centric with experience in C++
-- AI Student
-- Unity Experience
+Success response:
+{
+    "status" : "Success"
+}
 
----
-
-##### Richard Mcfadden
-- Experience in multiple languages
-- Facial Recognition experience
-- Interest in AI and Computer Networks
-
----
-
-##### Adrian le Grange
-- Experience with Unity
-- Easilty adaptable to programming languages
-- Interested in AI, graphics as well as operating systems
-
----
-
-##### Kyle Olivier
-- Experience in C# and Java
-- User experience focused
+Failure response:
+{
+    "status" : "Failure",
+    "message" : "some description of the error"
+}
 
 ---
 
-##### Alessio Rossi
-- Determined learner
-- Motivational member
-- Experienced in a number of programming languages
+#### /retrieveUser
+Endpoint to retrive a user's details based on given email. The request:
+
+Example:
+{
+    "email" : "email@some.domain"
+}
+
+Success response:
+{
+    "status" : "Success"
+    "email" : "email@some.domain",
+    "name" : "John",
+    "surname": "Doe",
+    "title": "Mr",
+    "fd": [123,...,456]
+}
+
+Failure response:
+{
+    "status" : "Failure",
+    "message" : "some description of the error"
+}
 
 ---
--- Database_Manager/databaseManager.js
+++ Database_Manager/databaseManager.js
@@ -1,6 +1,6 @@
 //Firbase setup
 var admin = require("firebase-admin");
-var serviceAccount = require("./credentials.json");
+var serviceAccount = require("./debug_credentials.json");
 admin.initializeApp(
     {
         credential: admin.credential.cert(serviceAccount),
@@ -12,28 +12,119 @@ let db = admin.firestore();
 var express = require('express');
 var app = express();
 
+var parser = require("body-parser");
+app.use(parser.urlencoded({extended : false}));
+
+//Helper functions
+function validateUserObject(request, response)
+{
+    var valid = true;
+    
+    //Check for email field
+    if(request.body.email == undefined || request.body.email.length < 1)
+    {
+        response.end(JSON.stringify(
+            {
+                "status" : "Failure",
+                "message" : "'email' field was not specified"
+            }
+        ));
+        valid = false;
+    }
+
+    //Check for name field
+    if(request.body.name == undefined || request.body.name.length < 1)
+    {
+        response.end(JSON.stringify(
+            {
+                "status" : "Failure",
+                "message" : "'name' field was not specified"
+            }
+        ));
+        valid = false;
+    }
+
+    //Check for surname field
+    if(request.body.surname == undefined || request.body.surname.length < 1)
+    {
+        response.end(JSON.stringify(
+            {
+                "status" : "Failure",
+                "message" : "'surname' field was not specified"
+            }
+        ));
+        valid = false;
+    }
+
+    //Check for title field
+    if(request.body.title == undefined || request.body.title.length < 1)
+    {
+        response.end(JSON.stringify(
+            {
+                "status" : "Failure",
+                "message" : "'title' field was not specified"
+            }
+        ));
+        valid = false;
+    }
+    
+    //Check for facial data field
+    if(request.body.fd == undefined || request.body.fd.length < 1)
+    {
+        response.end(JSON.stringify(
+            {
+                "status" : "Failure",
+                "message" : "'fd' field was not specified. If no facial data is known please indicate with '[]'"
+            }
+        ));
+        valid = false;
+    }
+
+    //Check for active field
+    if(request.body.active == undefined || request.body.active.length < 1)
+    {
+        response.end(JSON.stringify(
+            {
+                "status" : "Failure",
+                "message" : "'active' field was not specified."
+            }
+        ));
+        valid = false;
+    }
+
+    if(request.body.active != 'false' && request.body.active != 'true')
+    {
+        response.end(JSON.stringify(
+        {
+            "status" : "Failure",
+            "message" : "'active' field must be boolean ('true'/'false')"
+        }
+        ));
+        valid = false;
+    }
+
+    return valid;
+}
+
 //Endpoint for retrieving known email-facial data pairs (To be used for facial recognition)
-app.get('/getKnownFaceEncodings', function (req, res) {
+app.post('/retrieveEncodings', function (request, response) {
     //Get known face encodings from the DB with their respective email addresses
-    users = db.collection("Users").get()
+    users = db.collection("users").get()
         .then(userSet => {
         var emails = [];
         var faceData = [];
 
         userSet.forEach(user => {
-            emails.push(user.get("Email"));
-
-            //Since the data is saved in such a shit way we first strip the unnessesasary objects
-            var facialData = user.get("image_vector");
-            faceData.push(facialData[0].encoding);
-
-            //console.log("Retrieved " + user.id);
-            //console.log(facialData[0].encoding);
+            if(user.get("active"))
+            {
+                emails.push(user.get("email"));
+                faceData.push(user.get("fd"));
+            }
         });
         
         //Return JSON object with two arrays; One with faces and the other with emails to match
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(
+        response.setHeader('Content-Type', 'application/json');
+        response.end(JSON.stringify(
             {
                 "emails" : emails,
                 "fd" : faceData
@@ -42,8 +133,168 @@ app.get('/getKnownFaceEncodings', function (req, res) {
     });
 });
 
+app.post('/register', function (request, response) 
+{
+    if(!validateUserObject(request, response))
+    {
+        return; //Stop registration as information provided was not sufficient
+    }
+
+    //Check if user exists
+    var userRef = db.collection('users').doc(request.body.email);
+    var getDoc = userRef.get()
+        .then(doc => {
+            if (doc.exists) //User exists 
+            {
+                response.end(JSON.stringify(
+                    {
+                        "status" : "Failure",
+                        "message" : "Specified user already exists"
+                    }
+                ));
+                return;
+            } 
+            else //User does not exist
+            {
+                //Update
+                let updateDoc = db.collection('users').doc(request.body.email).set(
+                    {
+                        "email" : request.body.email,
+                        "name" : request.body.name,
+                        "surname" : request.body.surname,
+                        "title" : request.body.title,
+                        "fd" : JSON.parse(request.body.fd),
+                        "active" : JSON.parse(request.body.active)
+                    })
+                .then(ref => {
+                    console.log('Updated: ', request.body.email);
+                    response.end(JSON.stringify(
+                        {
+                            "status" : "Success"
+                        }));
+                });
+                return;
+            }
+        })
+        .catch(err => {
+            response.end(JSON.stringify(
+                {
+                    "status" : "Failure",
+                    "message" : "Document could not be retrieved"
+                }
+            ));
+            return;
+        });
+});
+
+app.post('/update', function (request, response)
+{
+    if(!validateUserObject(request, response))
+    {
+        return; //Stop update operation as information provided was not sufficient
+    }
+
+    //Check if user exists
+    var userRef = db.collection('users').doc(request.body.email);
+    var getDoc = userRef.get()
+        .then(doc => {
+            if (doc.exists) //User exists 
+            {
+                //Update
+                let updateDoc = db.collection('users').doc(request.body.email).set(
+                    {
+                        "email" : request.body.email,
+                        "name" : request.body.name,
+                        "surname" : request.body.surname,
+                        "title" : request.body.title,
+                        "fd" : JSON.parse(request.body.fd),
+                        "active" : JSON.parse(request.body.active)
+                    })
+                .then(ref => {
+                    console.log('Updated: ', request.body.email);
+                    response.end(JSON.stringify(
+                        {
+                            "status" : "Success"
+                        }));
+                });
+            } 
+            else //User does not exist
+            {
+                response.end(JSON.stringify(
+                    {
+                        "status" : "Failure",
+                        "message" : "Specified user does not exist"
+                    }
+                ));
+                return; //Stop as user does not exist 
+            }
+        })
+        .catch(err => {
+            response.end(JSON.stringify(
+                {
+                    "status" : "Failure",
+                    "message" : "Document could not be retrieved"
+                }
+            ));
+            return;
+        });
+});
+
+app.post('/retrieveUser', function (request, response)
+{
+    if(request.body.email == undefined || request.body.email.length < 1)
+    {
+        response.end(JSON.stringify(
+            {
+                "status" : "Failure",
+                "message" : "'email' field was not specified"
+            }
+        ));
+        return; //Stop update operation as information provided was not sufficient
+    }
+
+    //Check if user exists
+    var userRef = db.collection('users').doc(request.body.email);
+    var getDoc = userRef.get()
+        .then(doc => {
+            if (doc.exists) //User exists 
+            {
+                response.end(JSON.stringify(
+                    {
+                        "status" : "Success",
+                        "email" : doc.get("email"),
+                        "name" : doc.get("name"),
+                        "surname" : doc.get("surname"),
+                        "title" : doc.get("title"),
+                        "fd" : doc.get("fd")
+                    }
+                ));
+            } 
+            else //User does not exist
+            {
+                response.end(JSON.stringify(
+                    {
+                        "status" : "Failure",
+                        "message" : "Specified user does not exist"
+                    }
+                ));
+                return; //Stop as user does not exist 
+            }
+        })
+        .catch(err => {
+            response.end(JSON.stringify(
+                {
+                    "status" : "Failure",
+                    "message" : "Document could not be retrieved"
+                }
+            ));
+            return;
+        });
+});
+
 //Run server
-var server = app.listen(8081, function () {
+var server = app.listen(8081, function () 
+{
    var host = server.address().address
    var port = server.address().port
    console.log("Listening at http://%s:%s", host, port)
