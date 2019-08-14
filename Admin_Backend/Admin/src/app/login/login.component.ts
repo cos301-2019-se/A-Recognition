9b1074475948f7a3bb3ba0eecf7345cdaeb91ab9
Kyle <kolivier12@gmail.com>
2019-08-15 00:50:57 +0200

    Getting linux to run on windows

---
+++ Admin_Backend/Admin/src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators  } from '@angular/forms';
+import {AuthService} from '../auth.service';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(public authService: AuthService, private formBuilder: FormBuilder) 
+  { 
+
+  }
+
+  userLogin: FormGroup;
+
+  ngOnInit() 
+  {
+    this.userLogin = this.formBuilder.group(
+    {
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+}
