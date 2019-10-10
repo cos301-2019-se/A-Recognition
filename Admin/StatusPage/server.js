0f7dc246aae4c3b9e66eb04361880dd777489079
Adrian-le-Grange <u17056782@tuks.co.za>
2019-10-10 17:01:01 +0200

    Implemented server used to display status of authorization

---
+++ Admin/StatusPage/server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+const { exec } = require('child_process');
+
+app.get('/allow', function (req, res) {
+    exec('/usr/lib/firefox/firefox/firefox success.html', (err, stdout, stderr) => {
+        if (err)
+        {
+            console.log("Could not open the browser!");
+        }
+        return;
+    });
+    res.end();
+})
+
+app.get('/deny', function (req, res) {
+    exec('/usr/lib/firefox/firefox/firefox failure.html', (err, stdout, stderr) => {
+        if (err)
+        {
+            console.log("Could not open the browser!");
+        }
+        return;
+    });
+    res.end();
+ })
+
+var server = app.listen(42069, function ()
+{
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
